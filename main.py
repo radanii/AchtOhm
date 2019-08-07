ce61c443647012457fe8c2060db3b85199ff9348
Radanii <patrick.o.o@hotmail.de>
2019-08-07 20:07:17 +0200

    Jinja base html implemented

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    page_title = "Acht Ohm - Artist Page"
     return render_template("index.html")
 
 
 @app.route("/tracks")
 def tracks():
+    page_title = "Acht Ohm - Tracks"
+    page_background = "/static/img/pexels-photo-2425692.jpeg"
     return render_template("tracks.html")
+
 
 @app.route("/shop")
 def shop():
+    page_title = "Acht Ohm - Shop"
     return render_template("shop.html")
+
 
 @app.route("/contact")
 def contact():
+    page_title = "Acht Ohm - Contact"
     return render_template("contact.html")
 
 if __name__ == '__main__':
