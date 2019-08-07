e7b3f8f085c3b3ec6557062605d8eb46d38580f4
Radanii <patrick.o.o@hotmail.de>
2019-08-07 20:51:25 +0200

    minor fixes css

--- main.py
+++ main.py
@@ -5,26 +5,35 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     page_title = "Acht Ohm - Artist Page"
-    return render_template("index.html")
+    return render_template("index.html",
+                           page_title=page_title)
 
 
 @app.route("/tracks")
 def tracks():
     page_title = "Acht Ohm - Tracks"
     page_background = "/static/img/pexels-photo-2425692.jpeg"
-    return render_template("tracks.html")
+    return render_template("tracks.html",
+                           page_title=page_title,
+                           page_background=page_background)
 
 
 @app.route("/shop")
 def shop():
     page_title = "Acht Ohm - Shop"
-    return render_template("shop.html")
+    page_background = "/static/img/pexels-photo-2425692.jpeg"
+    return render_template("shop.html",
+                           page_title=page_title,
+                           page_background=page_background)
 
 
 @app.route("/contact")
 def contact():
     page_title = "Acht Ohm - Contact"
-    return render_template("contact.html")
+    page_background = "/static/img/pexels-photo-2425692.jpeg"
+    return render_template("contact.html",
+                           page_title=page_title,
+                           page_background=page_background)
 
 if __name__ == '__main__':
     app.run()
