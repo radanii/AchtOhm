04552f569489595706f0112d2fb1bacd31771056
Radanii <patrick.o.o@hotmail.de>
2019-08-11 15:15:54 +0200

    Heroku Deployment Database

--- models.py
+++ models.py
@@ -2,8 +2,7 @@ import os
 from sqla_wrapper import SQLAlchemy
 # from SQLAlchemy import create_engine
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@ec2-46-"
-                   "137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
+db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
 
 
 class User(db.Model):
