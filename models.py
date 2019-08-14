45cb19a050a1f2982ecbc9dc57fd2b638164de92
Radanii <patrick.o.o@hotmail.de>
2019-08-14 17:35:05 +0200

    herokupostgres

--- models.py
+++ models.py
@@ -5,8 +5,7 @@ from sqla_wrapper import SQLAlchemy
 
 # USE THIS TO CONNECT TO HEROKU DATABASE
 db = SQLAlchemy(os.getenv("DATABASE_URL",
-                          "postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@"
-                          "ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
+                          "postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
 #
 #
 # Implementation of config to use both at the same time will be happening with the manage.py
