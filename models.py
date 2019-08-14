8e4aaf3a5c8bb1907d3c4c62052e8e0ac5703000
Radanii <patrick.o.o@hotmail.de>
2019-08-14 17:40:11 +0200

    herokupostgres

--- models.py
+++ models.py
@@ -4,9 +4,7 @@ from sqla_wrapper import SQLAlchemy
 # db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
 
 # USE THIS TO CONNECT TO HEROKU DATABASE
-db = SQLAlchemy(os.getenv("DATABASE_URL",
-                          "postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
-#
+db = SQLAlchemy(os.getenv("DATABASE_URL","postgres://vbnuczkqpdmuaw:9f7036a79d94b51e3fedc899ca6e77b387f6b3ed4707d63e082d410e21d86afa@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/d26b2unj72ae4a"))
 #
 # Implementation of config to use both at the same time will be happening with the manage.py
 
